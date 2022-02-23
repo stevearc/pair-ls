a114200bb9abad437ce2a9a34637d039a469f765
Steven Arcangeli <stevearc@stevearc.com>
2022-02-22 16:06:07 -0800

    ci: hack around broken aws-cli

--- publish-static-site.sh
+++ publish-static-site.sh
@@ -2,5 +2,6 @@
 set -e
 CLOUDFRONT_DISTRIBUTION="E1DAF3Z7T3PIYW"
 
+aws --version
 aws s3 sync --debug --region us-west-1 ./static/ s3://code.stevearc.com/
 aws cloudfront create-invalidation --distribution-id "$CLOUDFRONT_DISTRIBUTION" --paths '/*'
